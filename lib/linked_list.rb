af71f9f2a871d1b91b1976f40b46011f394001e8
Yaz <yasmineoshaugh@gmail.com>
2020-05-07 15:39:43 -0700

    Completed, passing all tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,41 +5,73 @@ class LinkedList
 
   def initialize
     @head = nil
+    @length = 0
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    if head == nil 
+      @head = Node.new(data)
+    else
+      new_head = Node.new(data, head)
+      @head = new_head
+    end
+    @length += 1
+    return nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    if head != nil
+      return head.data
+    end
+      return head
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def length
-    return 0
+    return @length
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-
+    if head.nil?
+      add_first(data)
+      return
+    end
+    @length += 1
+    get_node_at_index(length).next = Node.new(data)
+    return nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n) 
+  # Space complexity - O(1)
   def get_last
-
+    return get_at_index(length)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
+    if index > length
+      return nil
+    end
+    target_node = get_node_at_index(index)
+    return target_node.data
+  end
 
+  private
+  def get_node_at_index(index)
+    current = head
+    index.times do 
+      if current.next != nil
+        current = current.next
+      end
+    end
+    return current
   end
 end
