f01e7ef8fa72c5ea8166d2865c5137b9f3e1a7e6
SuelyBarreto <suely@barreto.us>
2020-05-09 09:36:52 -0700

    linked list exercises passing all tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,36 +10,85 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    new_node = Node.new(data, @head)
+    @head = new_node
+    return @head
   end
+
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    if @head.nil?
+      return nil
+    else
+      return @head.data
+    end
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    count = 0
+    current = @head
+    while !current.nil?
+      count += 1
+      current = current.next
+    end
+    return count
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
+    last = get_last_node
+    if last.nil?
+      return add_first(data)
+    end
 
+    new_node = Node.new(data, nil)
+    last.next = new_node
+    return new_node 
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
+    last = get_last_node
+    if last.nil?
+      return nil
+    else
+      return last.data
+    end
+  end  
+  
+  # Time complexity - ?
+  # Space complexity - ?
+  def get_last_node
+    if @head.nil?
+      return nil
+    end
 
+    current = @head
+    while !current.next.nil?
+      current = current.next
+    end
+    return current
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    current_index = 0
+    current = @head
+    while (current_index < index && !current.nil?) do
+      current_index += 1
+      current = current.next
+    end
+    if current.nil?
+      return nil
+    else
+      return current.data
+    end
   end
 end
