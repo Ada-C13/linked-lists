80a5370258c8fc56c424a23d435d23f93770083a
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-10 16:59:09 -0700

    Implemented class methods on LinkedList class; tests passing.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,82 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    new_node = Node.new(data)
+    new_node.next = @head
+    @head = new_node
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if @head.nil?
+    return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the linked list
+  # Space complexity - O(1) (constant amount of work for counter variable)
   def length
-    return 0
+    return 0 if @head.nil? 
+    current = @head
+    count = 0
+
+    while (!current.nil?)
+      current = current.next
+      count += 1
+    end
+
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is number of nodes in the linked list
+  # Space complexity - O(1)
   def add_last(data)
+    if @head.nil?
+      self.add_first(data)
+      return
+    end
 
+    new_node = Node.new(data)
+    current = @head
+
+    while (!current.next.nil?)
+      current = current.next
+    end
+
+    current.next = new_node
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the linked list
+  # Space complexity - O(1)
   def get_last
+    return nil if @head.nil?
+    current = @head
 
+    while (!current.next.nil?)
+      current = current.next
+    end
+
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the linked list
+  # Space complexity - O(1)
   def get_at_index(index)
+    current_index = 0
+    current = @head
 
+    while (current_index < index && !current.nil?)
+      current_index += 1
+      current = current.next
+    end
+
+    if current.nil?
+      return nil
+    else
+      return current.data
+    end
   end
 end
