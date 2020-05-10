406231f8b3580c1073cb516eb5d4d2e159afd537
Catherina <catherine.pchelina@gmail.com>
2020-05-09 20:42:05 -0700

    Add LinkedList methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,79 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    if @head.nil? 
+      @head = Node.new(data)
+    else
+      @head = Node.new(data, @head)
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return @head.data if @head != nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    count = 0
+    current = @head
+    while current != nil
+      count += 1
+      current = current.next
+    end
+
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(n), where n is the number of nodes we're adding
   def add_last(data)
+    if @head.nil? 
+      @head = Node.new(data)
+    else
+    
+      current = @head 
+      while current.next != nil
+        current = current.next 
+      end 
 
+      current.next = Node.new(data)
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
+    current = @head
 
+    while current.next != nil
+      current = current.next
+    end
+
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
+    current_index = 0
+    current = @head 
 
+    while (current_index < index && !current.nil?)
+
+      current_index += 1
+      current = current.next  
+    end
+
+    if current.nil?
+      return nil
+    else
+      return current.data
+    end
   end
 end
