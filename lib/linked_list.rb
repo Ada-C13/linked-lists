83522922ac9103914295d7719fe54d86e42eb98c
quinqu <quinruby0@gmail.com>
2020-05-11 15:52:03 -0700

    get at index

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,16 +7,16 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(n) - n length of linked list 
+  # Space complexity - o(1)
   def add_first(data)
     new_head = Node.new(data)
     new_head.next = @head;
     @head = new_head;
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(1)
+  # Space complexity - o(1)
   def get_first
     if @head == nil 
       return nil
@@ -24,48 +24,41 @@ class LinkedList
     return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(n)
+  # Space complexity - o(1)
   def length
     if @head == nil 
       return 0
     end 
-
    current = @head
    count = 0
-
    while current != nil 
     current = current.next
     count = count + 1
-    puts current
    end 
 
    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(n)
+  # Space complexity - o(1)
   def add_last(data)
     if @head == nil 
       @head = Node.new(data)
       return 
     end 
-
     current = @head 
     next_node = @head.next
-
     while next_node != nil 
       current = next_node
       next_node = next_node.next
     end 
-    
     current.next = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(n)
+  # Space complexity - o(1)
   def get_last
-
     if @head == nil 
       return nil 
     end
@@ -75,14 +68,24 @@ class LinkedList
       current = next_node;
       next_node = next_node.next; 
     end 
-  
     return current.data;
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - o(n) - n is num of node until you reach index
+  # Space complexity - o(1)
   def get_at_index(index)
+    i = 0 
+    current = @head 
+    while i < index && current != nil 
+      i+=1
+      current = current.next
+    end 
 
+    if current.nil? 
+      raise ArgumentError
+    end
+   
+    return current.data
 
   end
 end
