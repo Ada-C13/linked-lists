71c94fc9de18331bfc7de3a0313a1433d9b23f1b
thenora <thenora@gmail.com>
2020-05-10 23:33:12 -0700

    passing tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative 'node'
+require 'pry'
 
 class LinkedList
   attr_reader :head
@@ -7,39 +8,79 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
-  def add_first(data)
-
+  # Time complexity - O(1)
+  # Space complexity - O(n)
+  def add_first(value)
+    node = Node.new(value, @head)
+    @head = node
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(n)
   def get_first
-
+    return @head.data if @head != nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(n)
   def length
-    return 0
+    counter = 0
+    current = head
+    until current.nil?
+      counter += 1
+      current = current.next
+    end
+    return counter
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(n)
   def add_last(data)
+    current = @head
+    newNode = Node.new(data, nil)
 
+    if current.nil?
+      @head = newNode
+    else
+      until current.next.nil?
+        current = current.next
+      end
+      current.next = newNode
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(n)
   def get_last
-
+    if @head.nil?
+      return nil
+    else 
+      current = @head
+      until current.next.nil?
+        current = current.next
+      end
+    end
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(n)
   def get_at_index(index)
+    current = @head
 
+    currentIndex = 0
+
+    while currentIndex < index && !current.nil?
+      currentIndex += 1
+      current = current.next
+    end
+
+    if current.nil?
+      return nil
+    end
+
+    return current.data
   end
+
 end
+
