f77b48fcb752caf71728ee86ffeba074546658d2
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-11 16:08:50 -0700

    adds comments about time and space complexity, refactors nil? using bang operator

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,26 +8,26 @@ class LinkedList
     @next = nextNode
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1), we will always be adding to the beginning of the list
+  # Space complexity - O(1)
   def add_first(data)
     @head = Node.new(data, @head)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1), only looking at @head
+  # Space complexity - O(1)
   def get_first
     return @head ? @head.data : nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the list
+  # Space complexity - O(1)
   def length
-    return 0 if @head.nil?
+    return 0 if !@head
     
     current = @head
     count = 0 
-    until current.nil?
+    until !current
       count += 1
       current = current.next
     end
@@ -35,32 +35,32 @@ class LinkedList
     return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the list
+  # Space complexity - O(1)
   def add_last(data)
-    return add_first(data) if @head.nil?
+    return add_first(data) if !@head
     
     current = @head
-    current = current.next until current.next.nil?
+    current = current.next until !current.next
     current.next = Node.new(data)
   end
 
-  # Time complexity - ?
+  # Time complexity - O(n), where n is the number of nodes in the list
   # Space complexity - ?
   def get_last
     current = @head
-    current = current.next until current.next.nil?
+    current = current.next until !current.next
     return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n), where n is the value of index
+  # Space complexity - O(1)
   def get_at_index(index)
     current = @head
     count = 0
     
     until count == index
-      return nil if current.nil?
+      return nil if !current
       current = current.next
       count += 1
     end
