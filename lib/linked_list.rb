89ea6c5ab67e8a00438a54c2ad546b14863eadd9
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-11 00:56:07 -0700

    finished assignment

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,36 +10,72 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    new_node = Node.new(data)
+    new_node.next = @head
+    @head = new_node
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    return nil if self.length < 1
+    return @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    count = 0
+    current = @head
+    while current != nil
+      count += 1
+      current = current.next
+    end
+    return count
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
+    if @head.nil?
+      add_first(data)
+      return
+    end
+
+    current = head
+    until current.next.nil?
+      current = current.next
+    end
+
+    current.next = Node.new(data)
 
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
+    current = head
+    until current.next.nil?
+      current = current.next
+    end
 
+    return current.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
+    current_index = 0 
+    current = @head
+    while (current_index < index && !current.nil?)
+      current_index += 1
+      current = current.next
+    end
 
+    if current.nil?
+      return nil
+    else
+      return current.data
+    end
   end
 end
