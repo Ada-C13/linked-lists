5ec7ae70bf9046faa88557ae892eab155bc53d93
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-10 22:36:43 -0700

    Wrote methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,83 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity O(1)
+  # Space complexity O(1)
   def add_first(data)
+    new_node = Node.new(data)
+    if @head = nil
+      @head = new_node
+    else 
+      new_node.next = @head
+      @head = new_node
+    end
 
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    if @head.nil?
+      return nil
+    else
+      return @head.data
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    current = @head
+    count = 0
+    if head == nil
+      return count
+    end
+    until current.nil?
+      current = current.next
+      count += 1
+    end
+    return count
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-
+    if @head.nil?
+      @head = Node.new(data)
+      return
+    end
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.next = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    if @head.nil?
+      return nil
+    end
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
-
+    if @head.nil?
+      return nil
+    end
+    current = @head
+    count = 0
+    until count == index || current.next.nil?
+      current = current.next
+      count += 1
+    end
+    return current.data
   end
 end
