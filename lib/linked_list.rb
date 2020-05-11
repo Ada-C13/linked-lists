a52580de03d9ced11d4978a6a84c0532d19601a3
kate myer <kate.d.mangubat@gmail.com>
2020-05-11 13:49:56 -0700

    Space - Kate

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,8 @@
 require_relative 'node'
+#o->o->o->o
+#head......tail, unidirectional 
+#current is whatever node we are on
+#remember to make a new node, assign it to another variable
 
 class LinkedList
   attr_reader :head
@@ -7,39 +11,80 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    new_node = Node.new(value, @head)
+    @head = new_node
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return @head ? @head.data : nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1) because this list isn't growing over time
   def length
-    return 0
+    length = 0
+    if !@head
+      return length
+    else
+      current = @head
+      while current
+        length += 1
+        current = current.next
+      end
+    end
+    return length
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-
+    new_node = Node.new(data)
+    if !@head
+      @head = new_node
+    else
+      current = @head
+    # while current.next
+    while current.next != nil
+      current = current.next
+    end
+    current.next = new_node
   end
+end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    # return nil unless @head
+    if @head == nil
+      return nil
+    end
+    current = @head
+    while current.next != nil 
+      current = current.next
+    end
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
-
+    # return nil unless @head
+    if @head == nil
+      return nil
+    end
+    current = @head
+    index.times do 
+      # return nil unless current.next
+      if current.next == nil #check if index is out of bounds
+        return nil
+      end
+      current = current.next
+    end
+    return current.data
   end
 end
