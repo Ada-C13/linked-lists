d89ff9fa3e9ae0f2c262140149c362fa4eaff8d5
vwhwang <vwhwang38@gmail.com>
2020-05-11 12:08:11 -0700

    link practice completed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,79 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_first(data)
-
+    @head =  Node.new(data, @head)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if @head.nil? 
+    return @head.data 
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
+    current = @head 
+    count = 0 
+    until current.nil? 
+      count += 1 
+      current = current.next 
+    end 
+    return count 
     return 0
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
 
+    if @head.nil?
+      @head = Node.new(data,@head)
+      return 
+    end 
+
+    current = @head 
+    until current.next.nil? 
+      current = current.next 
+    end 
+
+    current.next = Node.new(data)
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
+ 
+    current = @head 
 
+    until current.next.nil?
+      current = current.next
+    end 
+
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
+
+    current = @head 
+    count = 0
+    
+    until current.nil? 
+      if count == index 
+        return current.data 
+      end 
+      count += 1 
+      current = current.next 
+    end 
+
+    return nil 
+
 
   end
 end
